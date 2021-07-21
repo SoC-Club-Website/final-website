d827540175330591c8bdcadf5799e642f82108b7
anushka567 <anushkasingh2608@gmail.com>
2021-07-21 11:02:57 +0530

    fourth commit

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+
 class Post(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField()
@@ -18,9 +19,12 @@ class Post(models.Model):
         return self.likes.count()
     def total_dislikes(self):
         return self.dislikes.count()
-    
+
+    def total_comment(self):
+        return self.comment_set.count()
+
 class Comment(models.Model):
-    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comment_set')
     comment=models.TextField(max_length=400)
     date_posted=models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(User, on_delete=models.CASCADE, default="1")
@@ -33,3 +37,7 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+    
+
+
